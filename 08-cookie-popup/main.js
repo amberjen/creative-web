b6aa1bd66d40d63391b6e7e759ae19d3b609c30d
Amber Jen <amberjen226@gmail.com>
2022-02-27 01:38:06 +0800

    Split text animation

--- 08-cookie-popup/main.js
+++ 08-cookie-popup/main.js
@@ -1,14 +1,16 @@
+// gsap.registerPlugin();
+
 const tl = gsap.timeline({
   defaults: {
     duration: .5,
-    easing: 'power2.out'
+    easing: "power2.out"
   }
 });
 
 tl.fromTo(
   '.cookie-container', 
   { scale: 0 },
-  { scale: 1, ease: 'elastic.out(1, .9)', duration: 1 }
+  { scale: 1, ease: "elastic.out(1, .9)", duration: 1 }
 );
 
 tl.fromTo(
@@ -46,6 +48,23 @@ btn.addEventListener('click', () => {
     opacity: 0,
     y: 100,
     duration: .5,
-    ease: 'power3.in'
+    ease: "power3.in"
   })
 });
+
+// Split text animation (alternative)
+let logo = document.querySelector('.logo');
+let letters = logo.textContent.split('');
+
+logo.textContent = '';
+
+letters.forEach((letter) => {
+  logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+
+gsap.set('.letter', { display: 'inline-block' });
+gsap.fromTo(
+  '.letter', 
+  { y: '100%', opacity: 0 }, 
+  { y: 0, opacity: 1, delay: 1.25, stagger: .05, duration: .5, ease: 'back.out(3)'  }
+);
